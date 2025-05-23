87ef99a41ae530ea3730d595642721503ea89395
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2025-05-23 22:34:29 +0500

    revised basics of for loop in python

---
+++ folio3-Internship-Interview-Preparation/loop_questions.py
@@ -0,0 +1,13 @@
+# 1. 
+# Your task: Write code to print only even numbers from 1 to 20
+
+# for i in range(1, 21):    
+#     if (i % 2 == 0):
+#         print(i)
+    
+# 2.
+# Your task: Write code to print numbers from 10 to 1 using for loop
+
+for i in range(10 , 0 , -1 ):
+    print(i)
+ 
